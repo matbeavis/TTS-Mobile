66e2e7414e95e5fc2f1565d0fb3fc1afa0754236
Matheus Figueiredo Murta da Silva <matheus.murta322@gmail.com>
2023-06-14 21:27:47 -0300

    Projeto TTS mobile

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { TextToSpeech } from '@capacitor-community/text-to-speech';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+selector: 'app-home',
+templateUrl: 'home.page.html',
+styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+public textoLeitura = "";
+public idioma: string = 'en-US';
 
-  constructor() {}
+constructor() {
+}
 
+public setIdioma(op: number) {
+if (op == 1) {
+this.idioma = 'pt-BR';
+} else {
+this.idioma = 'en-US';
+}
+
+}
+
+public speackText() {
+TextToSpeech.speak({
+text: this.textoLeitura,
+lang: this.idioma,
+rate: 1.0,
+});
+}
 }
